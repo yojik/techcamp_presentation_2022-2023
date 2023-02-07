c9e5e2d5e86393f2b0386329faf366699486298b
yojik <yoji.kanno@gmail.com>
2023-02-07 00:29:17 +0000

    test

--- marp.sh
+++ marp.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-docker run --rm -v $PWD:/home/marp/app/ -e LANG=$LANG -e MARP_USER="$(id -u):$(id -g)"  marpteam/marp-cli  src/pages/presentation.md  --allow-local-files true --html --pdf
+docker run --rm -v $PWD:/home/marp/app/ -e LANG=$LANG -e MARP_USER="$(id -u):$(id -g)"  marpteam/marp-cli  src/pages/presentation.md  --allow-local-files true --pdf
+docker run --rm -v $PWD:/home/marp/app/ -e LANG=$LANG -e MARP_USER="$(id -u):$(id -g)"  marpteam/marp-cli  src/pages/presentation.md  --allow-local-files true --html
+
 
